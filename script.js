31dd38f4ab991c65409040a4ed3ab907ab5a600c
Chaitanya Narhare <chetannarhare111@gmail.com>
2024-03-26 23:24:20 +0530

    created initial files of  the project

---
+++ script.js
@@ -0,0 +1,11 @@
+const video = document.querySelector('video')
+
+let constraints = {
+  video: true,
+  audio: true
+}
+
+// navigator.mediaDevices.getUserMedia(constraints)
+//   .then((stream) => {
+//     video.srcObject = stream
+//   })
